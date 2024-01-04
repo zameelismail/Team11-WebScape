9e8b893c265967b6981f68e2b98c52a174e0f439
Kunal Kittur <kunalkittur23@gmail.com>
2024-01-04 18:00:07 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 // You can add JavaScript functionality as needed for interactivity
 // For example, form validation, interactive map functionality, etc.
 
-// Sample JavaScript code
-document.addEventListener('DOMContentLoaded', function() {
-    // Add your JavaScript code here
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    var i = 0;
+    var txt = 'Your partner in sustainable technology disposal';
+    var speed = 50;
+    function typeWriter() {
+        if (i < txt.length) {
+            document.getElementById("typer").textContent += txt.charAt(i);
+            i++;
+            setTimeout(typeWriter, speed);
+        }
+    }
+    typeWriter();
+    setInterval(()=>{
+        i=0;
+        document.getElementById("typer").textContent ="";
+        typeWriter();
+    },4000);
 });
+
